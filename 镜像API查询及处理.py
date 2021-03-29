95ac984f201a736d3d63a11d5be57f3536ae8748
xuxinyu2020 <67886523+xuxinyu2020@users.noreply.github.com>
2021-03-29 13:20:14 +0800

    Update 镜像API查询及处理.py

--- 镜像API查询及处理.py
+++ 镜像API查询及处理.py
@@ -19,19 +19,19 @@ url = "http://10.1.8.70:11000/os/image/v1/v2/images"
 
 headers = {"Content-Type": "application/json",
            "Accept": "application/json",
-           "X-Auth-Token": "gAAAAABgXcs-sGZ1guPuMsp3KN5QIx9EWhdmEMrGQpdUDCINOLH7W5yFX0veeCGcZjNPua4jTgvZpzzYFkrqr06eeRR7MzkiGZg4KN8dSckgcDHqUueqH0YaPFLfG0RiqGiQ2908VQ4XSvNcIos-V8LM9yQqK6MrHkDLs1AAsOZvDnvGtI03cpk"
+           "X-Auth-Token": "gAAAAABgYVJXgpV11sARIVnKZER0OSoMIZCDBK6kn-_nrfD4WpQg71O8VwT5s8m5oLU-hHXZguSwhLI0fCH5Wu8UHhv_pyd_2JfQNj-Y8P6SUc35rHRHp6PNhcIcDLjlW2NYAMhvJCMejgCn3WhIXI3MOQOHMHPC4Zb4H6pZK0M2-wIOkOiisEM"
            }
 
 
-def request_image(url):
-    '''请求镜像列表API获取json数据保存至文件'''
+def request_image(url,json_file_saved):
+    '''请求镜像列表API获取json数据保存至文件,url请求API，json_file_saved要保存的json文件名'''
     response = requests.get(url, headers=headers).json()
-    with open('images.json', 'w') as f:
+    with open(json_file_saved, 'w') as f:
         json.dump(response, f)
 
 
 def json_into_txt(file):
-    '''将json文件提取转化为txt文件'''
+    '''将json文件提取转化为txt文件，file要处理的json文件'''
     json_file = file
     with open(json_file, 'r', encoding='utf-8') as f2:
         response_json = json.load(f2)
@@ -51,8 +51,8 @@ def json_into_txt(file):
     print(f'成功完成{n}个镜像的信息导出！')
 
 
-def image_excel(file):
-    '''将镜像信息转化成excel表格'''
+def image_excel(file,excel_file_save):
+    '''将镜像信息转化成excel表格 file要处理的json文件名'''
     file_name = file
     wb = xlwt.Workbook()
     sheet = wb.add_sheet('glance镜像信息')
@@ -87,26 +87,26 @@ def image_excel(file):
             sheet.write(row + 1, col + 1, lines[row][col])
 
     # 保存数据
-    wb.save('glance镜像信息表.xls')
+    wb.save(excel_file_save)
     print("成功写入excel表格！")
 
 
-def generate_uuid_to_name():
-    '''根据镜像名字，查找到对应的uuid，并附加到前一列'''
+def generate_uuid_to_name(file_name):
+    '''根据镜像名字，查找到对应的uuid，并附加到前一列 file要遍历查询的镜像excel表'''
     # 遍历读取源excel里的镜像名，并保存为列表
     wb = xlrd.open_workbook('靶标、场景镜像名称.xls')
     sheets = wb.sheet_names()
     new_workbook = xlwt.Workbook()  # 新建一个工作簿对象，用于存放结果
+    file = file_name
     for sheet in sheets:
         list = []
         image_sheet = wb.sheet_by_name(sheet)
         print(f'{sheet}加载成功！')
-        print(range(1, image_sheet.nrows))
         for row in range(1, image_sheet.nrows):
-            list.append(image_sheet.cell(row, 1).value)
+            list.append(image_sheet.cell(row, 1).value.strip())
 
         # 在glance镜像表中遍历查找镜像名对应的uuid
-        glance_info_sheet = xlrd.open_workbook('鹏城实验室glance镜像信息查询/glance镜像信息表.xls').sheet_by_index(0)
+        glance_info_sheet = xlrd.open_workbook(file).sheet_by_index(0)
         uuid_to_name_sheet = new_workbook.add_sheet(sheet)  # 增加一个以原sheet为名的sheet
         for index, image_name in enumerate(list):
             flag = 0
@@ -121,15 +121,15 @@ def generate_uuid_to_name():
                 except:
                     continue
             if flag == 0:
-                print(f'表{sheet}序列号{index + 1}{image_name}未查询到uuid')
+                print(f'表{sheet} 序列号{index + 1} {image_name}未查询到uuid')
 
     new_workbook.save('镜像导出列表2.xls')
 
 
 def main():
-    # request_image(url)
-    # image_excel('images.json')
-    generate_uuid_to_name()
+    # request_image(url,'images2.json')
+    image_excel('images2.json','glance镜像信息表2.xls')
+    generate_uuid_to_name('glance镜像信息表2.xls')
 
 
 if __name__ == '__main__':
