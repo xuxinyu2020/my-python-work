473803d6424441bd1bb3218b8d8d7039b73d76c6
xuxinyu2020 <2690418353@qq.com>
2021-07-10 23:23:20 +0800

    使用数据库

---
+++ practice/36turtle.py
@@ -0,0 +1,20 @@
+from turtle import *
+
+width(4)
+forward(300)
+right(90)
+
+pencolor('red')
+forward(100)
+right(90)
+
+pencolor('green')
+forward(300)
+right(90)
+
+pencolor('blue')
+forward(100)
+right(90)
+
+
+done()
